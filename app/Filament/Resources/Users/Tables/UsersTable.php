8c79a149b328cc7b3bd25feaf29c30f9f3eb03cd
najehbedhief <najah.bedhief@gmail.com>
2025-12-24 21:08:26 +0100

    Add Filament + UserResource

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Tables\Table;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Tables\Columns\TextColumn;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('email')->searchable(),
+                TextColumn::make('email_verified_at')->dateTime()->sortable(),
+                TextColumn::make('created_at')->dateTime()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                // ViewAction::make(),
+                // EditAction::make(),
+                // DeleteAction::make()
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ]),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
